62b5bd032d33f050a1a0d531b9e3eb5f87716eb5
YunYeongChang <45313479+YunYeongChang@users.noreply.github.com>
2020-08-09 16:40:30 +0900

    feat : load editHistory of article or comment
    + load_editlog.js -> enable client get load object edit history.
    * new_article.js -> change status code
    * new_comment.js -> change status
    * swagger.yaml -> load_editlog node updated

--- routes/post/index.js
+++ routes/post/index.js
@@ -5,6 +5,7 @@ const router = Router();
 import new_article from './new_article';
 import new_comment from './new_comment';
 import load_comments from './load_comments';
+import load_editlogs from './load_editlogs';
 
 /**
  * RESTFUL API DOES NOT USE UNDERLINE IN URL
@@ -12,5 +13,6 @@ import load_comments from './load_comments';
 router.use('/new-article', new_article);
 router.use('/new-comment', new_comment);
 router.use('/load-comments', load_comments);
+router.use('/load-editlogs', load_editlogs);
 
 export default router;
