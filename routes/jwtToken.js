8bf587bb6c15dca0deff1f289886ea8e34cdf49c
YunYeongChang <45313479+YunYeongChang@users.noreply.github.com>
2020-09-06 03:06:15 +0900

    hotfix : change to response string instead of object

--- routes/jwtToken.js
+++ routes/jwtToken.js
@@ -1,15 +1,14 @@
 import { sign as _sign, verify as _verify } from "jsonwebtoken";
 
 const jwtSign = (object) => {
-    var _response = { "token" : null, "tokenerror" : true };
+    var _response = { "jwttoken" : null, "tokenerror" : true };
     try {
         const signed = _sign(object.toJSON(), process.env.JWT_TOKE1N_SECRETKEY, {expiresIn: "2h"});
-        _response.token = signed;
+        _response.jwttoken = signed;
         _response.tokenerror = null;
         return _response;
     } catch (err) {
-        _response.token = null;
-        _response.tokenerror = err;
+        _response.tokenerror = err.toString();
         return _response;
     }
 };
@@ -17,13 +16,11 @@ const jwtSign = (object) => {
 const jwtVerify = (token, secret) => {
     var _response = { "verified" : null, "tokenerror" : true };
     try {
-        const verified = _verify(token, !secret ? process.env.JWT_TOKEN_SECRETKEY : secret);
-        _response.verified = verified;
+        _response.verified = _verify(token, !secret ? process.env.JWT_TOKEN_SECRETKEY : secret);
         _response.tokenerror = null;
         return _response;
     } catch (err) {
-        _response.token = null;
-        _response.tokenerror = err;
+        _response.tokenerror = err.toString();
         return _response;
     }
 };
