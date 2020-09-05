172a70446353df2b5fdd0418eb340bb5a09da88c
YunYeongChang <45313479+YunYeongChang@users.noreply.github.com>
2020-09-06 02:45:20 +0900

    feat : change return system to response with object
    hotfix : change to response string instead of object
    
    chore : install regex-safe to check regex string
    
    chore : fix code to load app logo from dev server

--- routes/jwtToken.js
+++ routes/jwtToken.js
@@ -1,20 +1,27 @@
 import { sign as _sign, verify as _verify } from "jsonwebtoken";
 
 const jwtSign = (object) => {
+    var _response = { "jwttoken" : null, "tokenerror" : true };
     try {
-        const signed = _sign(object.toJSON(), process.env.JWT_TOKEN_SECRETKEY, {expiresIn: "2h"});
-        return signed;
+        const signed = _sign(object.toJSON(), process.env.JWT_TOKE1N_SECRETKEY, {expiresIn: "2h"});
+        _response.jwttoken = signed;
+        _response.tokenerror = null;
+        return _response;
     } catch (err) {
-        return false;
+        _response.tokenerror = err.toString();
+        return _response;
     }
 };
   
 const jwtVerify = (token, secret) => {
+    var _response = { "verified" : null, "tokenerror" : true };
     try {
-        const verified = _verify(token, !secret ? process.env.JWT_TOKEN_SECRETKEY : secret);
-        return verified;
+        _response.verified = _verify(token, !secret ? process.env.JWT_TOKEN_SECRETKEY : secret);
+        _response.tokenerror = null;
+        return _response;
     } catch (err) {
-        return false;
+        _response.tokenerror = err.toString();
+        return _response;
     }
 };
 
