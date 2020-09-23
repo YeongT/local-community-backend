424e521fcd2a829eee1430f15f4cb8d6af4c7ad6
YunYeongChang <45313479+YunYeongChang@users.noreply.github.com>
2020-09-23 21:52:44 +0900

    feat : change user schema and apply to nodes

--- models/user.js
+++ models/user.js
@@ -1,23 +1,41 @@
 import { model, Schema } from "mongoose";
 
 const User = new Schema({
-  email: String,
-  password: String,
-  name: String,
-  gender: Number,
-  phone: String,
-  areaString: String,
-  lastlogin: String,
-  salt: String,
-  community: Array,
-  profileimg: String,
-  enable: {
-    type: Boolean,
-    default: false
+  account: {
+    email: String,
+    status: {
+      type: String,
+      default: "unknown"
+    },
+    joined: String
+  },
+  service: {
+    community: {
+      type: Array,
+      default: []
+    }
+  },
+  profile: {
+    imageurl: String,
+    name: String,
+    phone: String,
+    gender: Number,
+    areaString: String
+  },
+  auth: {
+    password: String,
+    denied: {
+      type: Number,
+      default: 0
+    },
+    history: {
+      lastlogin: String,
+      lastpwd: String
+    },
+    salt: String
   }
 }, {
   versionKey: false
 });
-
 
 export default model("user", User);
